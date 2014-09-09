c107fc6991a2322a0393837f38d6f43b92a0c0b1
guru <anjanaya@gmail.com>
2014-09-10 00:24:23 +0530

    s

--- src/main/java/com/rhcloud/wicket/WicketApplication.java
+++ src/main/java/com/rhcloud/wicket/WicketApplication.java
@@ -1,12 +1,19 @@
 package com.rhcloud.wicket;
 
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 public class WicketApplication extends WebApplication {
 
 	@Override
 	public Class<SimplePage> getHomePage() {
 		return SimplePage.class;
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		getComponentInstantiationListeners().add(new SpringComponentInjector(this));
 	}
 
 }
